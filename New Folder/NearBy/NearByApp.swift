b412ed903e191186c064f195683fa61a06fd073a
Rafat <rafat514@hotmail.com>
2026-01-31 22:01:30 -0500

    Initial project structure

---
+++ New Folder/NearBy/NearByApp.swift
@@ -0,0 +1,17 @@
+//
+//  NearByApp.swift
+//  NearBy
+//
+//  Created by Rafat on 2026-01-31.
+//
+
+import SwiftUI
+
+@main
+struct NearByApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
