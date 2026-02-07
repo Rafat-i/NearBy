a4fc8e664ea8e955311dfb0935b42172671d3157
Mel Tran <alphxrizen@gmail.com>
2026-02-07 11:59:04 -0500

    Update MapView: Annotations
    Temp static pins

--- NearBy/Views/MapView.swift
+++ NearBy/Views/MapView.swift
@@ -8,17 +8,40 @@
 import SwiftUI
 import MapKit
 
+
 struct MapView: View {
     //Will become a state variable to update realtime as the user moves
-    let cameraPosition: MapCameraPosition = .region(.init(center: .init(latitude: 37.3346, longitude: -122.0090), latitudinalMeters: 1300, longitudinalMeters: 1300))
-    
-    
+    let cameraPosition: MapCameraPosition = .automatic
+
+    //Temp places
+    struct Places: Identifiable {
+        let id = UUID()
+        let name: String
+        let coordinate: CLLocationCoordinate2D
+        let systemImage: String
+        let color: Color
+    }
+    let places : [Places] = [
+        .init(name: "LaSalle College", coordinate: .laSalleCollege, systemImage: "graduationcap.fill", color: .red),
+        .init(name: "McGill University", coordinate: .mcGill, systemImage: "graduationcap.fill", color: .red),
+        .init(name: "Mount Royal", coordinate: .mountRoyal, systemImage: "mountain.2.fill", color: .green),
+        .init(name: "Bell Centre", coordinate: .bellCentre, systemImage: "figure.hockey", color: .blue)
+    ]
     
     var body: some View {
         ZStack{
             Map(initialPosition: cameraPosition){
-                Marker("AppleHQ", systemImage: "laptopcomputer", coordinate: .appleHQ)
-                Marker("AppleHQ", systemImage: "laptopcomputer", coordinate: .appleHQ)
+                
+                ForEach(places) { place in
+                        Annotation(place.name, coordinate: place.coordinate) {
+                            Image(systemName: place.systemImage)
+                                .resizable()
+                                .foregroundStyle(.white)
+                                .frame(width: 20, height: 20)
+                                .padding(8)
+                                .background(place.color.gradient, in: .circle)
+                        }
+                    }
             }
             VStack{
                 SearchBarView()
@@ -49,8 +72,12 @@ struct MapView: View {
 }
 
 
-//temp markers data
+//temp cordinates
 extension CLLocationCoordinate2D{
+    static let laSalleCollege = CLLocationCoordinate2D(latitude: 45.4915, longitude: -73.5815)
+    static let mcGill = CLLocationCoordinate2D(latitude: 45.5048, longitude: -73.5772)
+    static let mountRoyal = CLLocationCoordinate2D(latitude: 45.5071, longitude: -73.5875)
+    static let bellCentre = CLLocationCoordinate2D(latitude: 45.4961, longitude: -73.5695)
+    static let museumOfFineArts = CLLocationCoordinate2D(latitude: 45.4987, longitude: -73.5794)
     
-    static let appleHQ = CLLocationCoordinate2D(latitude: 37.3346, longitude: -122.0097)
 }
