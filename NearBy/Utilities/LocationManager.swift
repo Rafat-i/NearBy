74811172fdcef706fb94fc4a19d2a019b1819cf3
Rafat <rafat514@hotmail.com>
2026-02-16 19:51:19 -0500

    Cleaned repository and updated map/location features

--- NearBy/Utilities/LocationManager.swift
+++ NearBy/Utilities/LocationManager.swift
@@ -5,25 +5,90 @@
 //  Created by Rafat on 2026-02-07.
 //
 
-
 import Foundation
+import MapKit
 import CoreLocation
 import Combine
 
 class LocationManager: NSObject, ObservableObject, CLLocationManagerDelegate {
+    
     private let manager = CLLocationManager()
+    
+    @Published var userLocation: CLLocationCoordinate2D?
     @Published var authorizationStatus: CLAuthorizationStatus?
+    @Published var locationError: String?
+    
     
     override init() {
         super.init()
         manager.delegate = self
+        manager.desiredAccuracy = kCLLocationAccuracyBest
+        manager.requestWhenInUseAuthorization()
+        manager.startUpdatingLocation()
     }
+    
     
     func requestPermission() {
         manager.requestWhenInUseAuthorization()
     }
     
+    func startUpdating() {
+        manager.startUpdatingLocation()
+    }
+    
+    func stopUpdating() {
+        manager.stopUpdatingLocation()
+    }
+    
+    
+    func locationManager(
+        _ manager: CLLocationManager,
+        didUpdateLocations locations: [CLLocation]
+    ) {
+        guard let latestLocation = locations.last else { return }
+        
+        DispatchQueue.main.async {
+            self.userLocation = latestLocation.coordinate
+            self.locationError = nil
+        }
+    }
+    
     func locationManagerDidChangeAuthorization(_ manager: CLLocationManager) {
-        authorizationStatus = manager.authorizationStatus
+        DispatchQueue.main.async {
+            self.authorizationStatus = manager.authorizationStatus
+        }
+        
+        switch manager.authorizationStatus {
+        case .notDetermined:
+            manager.requestWhenInUseAuthorization()
+            
+        case .restricted, .denied:
+            DispatchQueue.main.async {
+                self.locationError = "Location access denied. Please enable in Settings."
+            }
+            print("Location access denied")
+            
+        case .authorizedAlways, .authorizedWhenInUse:
+            manager.startUpdatingLocation()
+            DispatchQueue.main.async {
+                self.locationError = nil
+            }
+            print("Location access granted")
+            
+        @unknown default:
+            break
+        }
+    }
+    
+    func locationManager(
+        _ manager: CLLocationManager,
+        didFailWithError error: Error
+    ) {
+        print("Failed to find user's location: \(error.localizedDescription)")
+        
+        DispatchQueue.main.async {
+            self.locationError = error.localizedDescription
+        }
+        
     }
 }
